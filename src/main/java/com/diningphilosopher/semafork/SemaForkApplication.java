54a56dc0d206c67ee1f8236c8a786a2413ca6c8c
Flubberschnub <kw6561@gmail.com>
2026-02-23 21:06:25 -0600

    Initial commit

---
+++ src/main/java/com/diningphilosopher/semafork/SemaForkApplication.java
@@ -0,0 +1,13 @@
+package com.diningphilosopher.semafork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemaForkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemaForkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diningphilosopher/semafork/SemaForkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningphilosopher.semafork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemaForkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
