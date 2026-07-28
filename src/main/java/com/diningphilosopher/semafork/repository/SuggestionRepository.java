6946e49516f4cca386591cfab5a3ea67d50213dc
Flubberschnub <kw6561@gmail.com>
2026-07-28 11:17:14 -0500

    Add suggestion count query

--- src/main/java/com/diningphilosopher/semafork/repository/SuggestionRepository.java
+++ src/main/java/com/diningphilosopher/semafork/repository/SuggestionRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
     List<Suggestion> findAllByPartyIdOrderByCreatedAtAsc(Long partyId);
-    List<Suggestion> findAllByMemberIdOrderByCreatedAtAsc(Long memberId);
+    long countByPartyId(Long partyId);
 }
