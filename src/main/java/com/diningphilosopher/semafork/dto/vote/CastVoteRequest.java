0b0187523e7c794ded96fa76a74abe7e47b5ab1a
Flubberschnub <kw6561@gmail.com>
2026-07-28 11:18:36 -0500

    Derive voter from participant token

--- src/main/java/com/diningphilosopher/semafork/dto/vote/CastVoteRequest.java
+++ src/main/java/com/diningphilosopher/semafork/dto/vote/CastVoteRequest.java
@@ -3,7 +3,6 @@ package com.diningphilosopher.semafork.dto.vote;
 import jakarta.validation.constraints.NotNull;
 
 public record CastVoteRequest(
-        @NotNull Long memberId,
         @NotNull Long suggestionId
 ) {
 }
