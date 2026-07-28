a8bdec36b1d12809de62b147a04d1718a184143d
Flubberschnub <kw6561@gmail.com>
2026-07-28 11:17:33 -0500

    Join parties by shareable code

--- src/main/java/com/diningphilosopher/semafork/dto/party/JoinPartyRequest.java
+++ src/main/java/com/diningphilosopher/semafork/dto/party/JoinPartyRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record JoinPartyRequest(
+        @NotBlank @Size(max = 8) String joinCode,
         @NotBlank @Size(max = 80) String memberName
 ) {
 }
