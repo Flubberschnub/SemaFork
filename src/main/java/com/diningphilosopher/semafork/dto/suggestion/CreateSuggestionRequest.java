2d1493a359c279233f4dda84b05d23c8a032ef22
Flubberschnub <kw6561@gmail.com>
2026-07-28 11:18:26 -0500

    Derive suggestion author from participant token

--- src/main/java/com/diningphilosopher/semafork/dto/suggestion/CreateSuggestionRequest.java
+++ src/main/java/com/diningphilosopher/semafork/dto/suggestion/CreateSuggestionRequest.java
@@ -1,11 +1,9 @@
 package com.diningphilosopher.semafork.dto.suggestion;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateSuggestionRequest(
-        @NotNull Long memberId,
         @NotBlank @Size(max = 120) String name
 ) {
 }
